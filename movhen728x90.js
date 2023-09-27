2d6eedf14947e7c1a696ea8437fcf90cfd14cd1f
movie <145183125+webmovi@users.noreply.github.com>
2023-09-27 13:00:25 +0700

    Create movhen728x90.js

---
+++ movhen728x90.js
@@ -0,0 +1,9 @@
+atOptions = {
+		'key' : '71782d789edf27680fa8193d5335e314',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/71782d789edf27680fa8193d5335e314/invoke.js"></scr' + 
+                 'ipt>');
